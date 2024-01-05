dc9d99e662c640a5326cec49704ec27abf637061
idris lawal <idriseun222@gmail.com>
2024-01-05 22:45:18 +0100

    init

---
+++ src/components/Table.vue
@@ -0,0 +1,42 @@
+<template>
+<h2>HTML Table</h2>
+
+<table>
+  <tr>
+    <th>Id</th>
+    <th>Name</th>
+  </tr>
+  <tr v-for="item in items" :key="item.id">
+    <td>
+        {{ item.id }}
+    </td>
+    <td>
+        {{ item.name }}
+    </td>
+  </tr>
+</table>
+</template>
+
+<style>
+    table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
+
+    td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    }
+
+    tr:nth-child(even) {
+    background-color: #dddddd;
+    }
+</style>
+
+<script>
+export default {
+    props: ['items']
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Table from '../components/Table.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <Table :items="products"/>
+  </main>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    data() {
+      return {
+        products: [
+          {
+            "id": 1,
+            "name": "Biscuit"
+          },
+          {
+            "id": 2,
+            "name": "Cookies"
+          }
+        ]
+      }
+    }
+}
+</script>
